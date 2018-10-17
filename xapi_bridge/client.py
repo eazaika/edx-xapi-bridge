f5ea38aa188135cd58c20c7c6c6b89ee2e0580c0
Bryan Wilson <bryanlandia@gmail.com>
2018-10-17 15:52:59 -0700

    Remote LRS client use either username+pw or auth hash, don't pass both

--- xapi_bridge/client.py
+++ xapi_bridge/client.py
@@ -11,12 +11,13 @@ from xapi_bridge import settings
 
 kw = {
     'endpoint': settings.LRS_ENDPOINT,
-    'username': settings.LRS_USERNAME,
-    'password': settings.LRS_PASSWORD
 }
 if settings.LRS_BASICAUTH_HASH:
     kw['auth'] = settings.LRS_BASICAUTH_HASH
+else:
+    kw['username'] = settings.LRS_USERNAME
+    kw['password'] = settings.LRS_PASSWORD
 
 # RemoteLRS will use auth if passed otherwise BasicAuth with un/pw
-
+import pdb; pdb.set_trace()
 lrs = RemoteLRS(**kw)
