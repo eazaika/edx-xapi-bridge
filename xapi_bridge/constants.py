f566f71860ae154e15f678f5bc4478e66db07d71
eazaika <w-en.d@yandex.ru>
2021-06-28 10:52:24 +0300

    add new constants

--- xapi_bridge/constants.py
+++ xapi_bridge/constants.py
@@ -1,34 +1,36 @@
 """Constants for xAPI vocabulary."""
 
 # xAPI verbs
+XAPI_VERB_ATTACHED = "http://activitystrea.ms/schema/1.0/attach"
 XAPI_VERB_ATTEMPTED = "http://adlnet.gov/expapi/verbs/attempted"
 XAPI_VERB_ANSWERED = "http://adlnet.gov/expapi/verbs/answered"
-# XAPI_VERB_FAILED = "http://adlnet.gov/expapi/verbs/failed"
+XAPI_VERB_FAILED = "http://adlnet.gov/expapi/verbs/failed"
 # XAPI_VERB_PASSED = "http://adlnet.gov/expapi/verbs/passed"
+XAPI_VERB_WATCHED = "http://activitystrea.ms/schema/1.0/watch"
+XAPI_VERB_EXITED = "http://adlnet.gov/expapi/verbs/exited"
 XAPI_VERB_LAUNCHED = "http://adlnet.gov/expapi/verbs/launched"
 XAPI_VERB_REGISTERED = "http://adlnet.gov/expapi/verbs/registered"
 XAPI_VERB_UNREGISTERED = "http://id.tincanapi.com/verb/unregistered"
 XAPI_VERB_COMPLETED = "http://adlnet.gov/expapi/verbs/completed"
-XAPI_VERB_EXPERIENCED = "http://adlnet.gov/expapi/verbs/experienced"
+XAPI_VERB_EXPERIENCED = "http://activitystrea.ms/schema/1.0/experience"
 
 XAPI_VERB_INITIALIZED = "http://adlnet.gov/expapi/verbs/initialized"
-XAPI_VERB_PLAYED = "https://w3id.org/xapi/video/verbs/played"
-XAPI_VERB_PAUSED = "https://w3id.org/xapi/video/verbs/paused"
-XAPI_VERB_SEEKED = "https://w3id.org/xapi/video/verbs/seeked"
-XAPI_VERB_INTERACTED = "http://adlnet.gov/expapi/verbs/interacted"
+XAPI_VERB_PLAYED = "http://activitystrea.ms/schema/1.0/play"
+XAPI_VERB_PAUSED = "http://id.tincanapi.com/verb/paused"
+XAPI_VERB_INTERACTED = "http://activitystrea.ms/schema/1.0/interact"
 
 # xAPI activities
 XAPI_ACTIVITY_QUESTION = 'http://adlnet.gov/expapi/activities/question'
 XAPI_ACTIVITY_INTERACTION = 'http://adlnet.gov/expapi/activities/interaction'
 XAPI_ACTIVITY_COURSE = 'http://adlnet.gov/expapi/activities/course'
-XAPI_ACTIVITY_MODULE = 'http://adlnet.gov/expapi/activities/module'
-XAPI_ACTIVITY_VIDEO = 'https://w3id.org/xapi/video/activity-type/video'
+XAPI_ASSESSMENT_MODULE = 'http://adlnet.gov/expapi/activities/assessment'
+XAPI_ACTIVITY_VIDEO = 'http://activitystrea.ms/schema/1.0/video'
 XAPI_ACTIVITY_LINK = 'http://adlnet.gov/expapi/activities/link'
 XAPI_ACTIVITY_POSITION = 'http://id.tincanapi.com/extension/position'
 
 # xAPI context
 XAPI_CONTEXT_REFERRER = "http://id.tincanapi.com/extension/referrer"
-XAPI_CONTEXT_VIDEO_LENGTH = "https://w3id.org/xapi/video/extensions/length"
+XAPI_CONTEXT_VIDEO_LENGTH = "http://id.tincanapi.com/extension/duration"
 XAPI_CONTEXT_VIDEO_CC_LANGUAGE = "https://w3id.org/xapi/video/extensions/cc-subtitle-lang"
 XAPI_CONTEXT_STARTING_POSITION = "http://id.tincanapi.com/extension/starting-position"
 XAPI_CONTEXT_ENDING_POSITION = "http://id.tincanapi.com/extension/ending-point"
